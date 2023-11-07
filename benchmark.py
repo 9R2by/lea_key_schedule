9134655250b4364003faa9838d5aaddfac277fa9
9R2by <37716834+9R2by@users.noreply.github.com>
2023-11-07 03:52:52 +0100

    update benchmark

--- benchmark.py
+++ benchmark.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 if __name__ == '__main__':
-    path_to_binary = './target/lea_encryption.exe'
+    path_to_binary = './target/lea_key_schedule'
     fig, ax = plt.subplots()
     output_text = ''
-    n = pow(2, 10)
+    n = pow(2, 12)
     median_arr = []
 
     for _ in tqdm(range(n + 1), desc="Loading", unit="Runs"):
@@ -21,10 +21,10 @@ if __name__ == '__main__':
         cycles = int(stdout.split()[0])
         median_arr.append(cycles)
 
-    ax.plot(signal.medfilt(median_arr), label=f'Median filtered encryption algorithm cycles per run')
+    ax.plot(signal.medfilt(median_arr), label=f'Median filtered key schedule algorithm cycles per run')
     ax.set_xlabel('n')
     ax.set_ylabel('Cycles')
-    ax.set_title('LEA Encryption')
+    ax.set_title('LEA Key Schedule')
     ax.legend()
     plt.savefig('output_plot.svg', format='svg')
     print(statistics.median(median_arr))
---
+++ run_benchmark.sh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf ./env
+
+#pip install virtualenv
+#python3-virtualenv or equivalent package needed!
+python3 -m venv env
+source env/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+python3 main.py
