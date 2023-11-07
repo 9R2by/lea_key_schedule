ae8637ca2e32d00f296d7d10c8935153d91ab867
9R2by <37716834+9R2by@users.noreply.github.com>
2023-11-07 01:49:45 +0100

    Update build_script_asm.sh

--- asm/build_script_asm.sh
+++ asm/build_script_asm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-clang main_with_cycles.c -O3 -S -masm=intel -march=icelake-client -Wall -Wextra -Wpedantic -fno-asynchronous-unwind-tables -fno-dwarf2-cfi-asm -o main_with_cycles.asm
-clang main.c -O3 -S -masm=intel -march=icelake-client -Wall -Wextra -Wpedantic -fno-asynchronous-unwind-tables -fno-dwarf2-cfi-asm -o main.asm
+clang main_with_cycles.c -O3 -S -masm=intel -march=native -Wall -Wextra -Wpedantic -fno-asynchronous-unwind-tables -fno-dwarf2-cfi-asm -o main_with_cycles.asm
+clang main.c -O3 -S -masm=intel -march=native -Wall -Wextra -Wpedantic -fno-asynchronous-unwind-tables -fno-dwarf2-cfi-asm -o main.asm
